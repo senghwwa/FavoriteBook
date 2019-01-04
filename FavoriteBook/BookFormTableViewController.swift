1293ac738617a4fb4c101f428875060b59e36cd4
Seng Hwwa <senghwwa@zana-girl.local>
2019-01-04 13:45:21 +0800

    Before removing original form view controller

--- FavoriteBook/BookFormTableViewController.swift
+++ FavoriteBook/BookFormTableViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class BookFormTableViewController: UITableViewController {
     
+    struct PropertyKeys {
+        static let unwind = "UnwindToBookTable"
+    }
+  
+    var book: Book?
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var authorTextField: UITextField!
     @IBOutlet weak var genreTextField: UITextField!
@@ -16,10 +22,28 @@ class BookFormTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateView()
+    }
+    
+    func updateView() {
+        guard let book = book else {return}
+        
+        titleTextField.text = book.title
+        authorTextField.text = book.author
+        genreTextField.text = book.genre
+        lengthTextField.text = book.length
     }
 
     @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let title = titleTextField.text,
+            let author = authorTextField.text,
+            let genre = genreTextField.text,
+            let length = lengthTextField.text else {return}
+        
+        book = Book(title: title, author: author, genre: genre, length: length)
+        
+        performSegue(withIdentifier: PropertyKeys.unwind, sender: self)
+
     }
     
 }
